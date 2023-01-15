9412e950de538850341b31b24ee7bad604049de5
Zero <samuelbustos10903@gmail.com>
2023-01-15 16:34:54 -0300

    fix: corrección del app.js

--- src/app.js
+++ src/app.js
@@ -1,13 +1,5 @@
 const menu = document.getElementById("menu");
-const links = document.getElementsByClassName("menu__links");
 
 window.onload = () => {
-  //   alert("Pagina cargada");
   menu.style.transform = "translateY(0)";
-  links.style.transform = "translateY(200%)";
 };
-
-// menu.addEventListener("load", () => {
-//   alert("pagina cargada");
-//   //   menu.style.transform.translate = 0;
-// });
